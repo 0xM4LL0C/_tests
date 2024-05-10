a9b11d80bfce90a3c4b1a7e24c86967192b6776c
hamletsargsyan <hamlets849@gmail.com>
2024-05-10 15:11:44 +0400

    Правки

--- tools/send_release_notification.py
+++ tools/send_release_notification.py
@@ -1,17 +1,26 @@
 import os
+import sys
+import requests
 from telebot import TeleBot
 
-
+def get_github_release_info(version):
+    url = f"  https://api.github.com/repos/HamletSargsyan/_tests/releases/tags/{version}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        release_info = response.json()
+        return release_info
+    response.raise_for_status()
+    
 
 def send_release_notification():
     bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
     chat_id = os.getenv("CHAT_ID")
-    release_type = os.getenv("GITHUB_EVENT_NAME")
+    
     release_version = os.getenv("GITHUB_REF").split("/")[-1]
-    release_text = os.getenv("RELEASE_BODY")
-    release_url = f"{os.getenv('GITHUB_SERVER_URL')}/{os.getenv('GITHUB_REPOSITORY')}/releases/{os.getenv('GITHUB_RUN_ID')}"
-
-    message = f"{release_type} {release_version}\n\n{release_text}\n\n{release_url}"
+    release = get_github_release_info(release_version) # type: dict
+        
+    message = (f"<b>{release.get('name')}</b>\n\n"
+               f"<i>{release.get('body')}</i>")
     
     bot = TeleBot(bot_token)
     bot.send_message(chat_id=chat_id, text=message)
